ffe07a2d1a9fe9f1895a4a006882f1be26548607
Bulat <sadikovba@gmail.com>
2020-08-26 18:46:47 +0300

    Added State, onLabelClick eventListener

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,17 +1,54 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false,
+  };
+
+  onLabelClick = () => {
+    this.setState({
+      done: !this.state.done,
+    });
+  };
+
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = `todo-list-item`;
+    if (done) {
+      classNames += ` done`;
+    }
 
     const style = {
-      color: important ? "tomato" : "black",
+      color: important ? `steelblue` : `black`,
+      fontWeight: important ? `bold` : `normal`,
     };
 
     return (
-      <span className="todo-list-item" style={style}>
-        {label}
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelClick}
+        >
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
     );
   }
