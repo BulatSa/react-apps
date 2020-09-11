d721956d3889025c3b76512a3525edac31753eec
Bulat <sadikovba@gmail.com>
2020-09-11 15:05:51 +0300

    Added PeoplePage component, refactoring

--- stardb/src/components/app/app.js
+++ stardb/src/components/app/app.js
@@ -2,35 +2,17 @@ import React, { Component } from "react";
 import "./app.css";
 
 import Header from "../header";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
 import RandomPlanet from "../random-planet";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
-  state = {
-    selectedPerson: null,
-  };
-
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id,
-    });
-  };
-
   render() {
     return (
       <div className="container">
         <Header />
         <RandomPlanet />
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
